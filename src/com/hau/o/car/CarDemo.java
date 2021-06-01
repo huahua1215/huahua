c8089c4b0cebab8a80635712f2eff35cc440c160
huahua1215 <s06u.vm0clgj9@gmail.com>
2021-06-01 16:45:03 +0800

    homework

--- src/com/hau/o/car/CarDemo.java
+++ src/com/hau/o/car/CarDemo.java
@@ -4,6 +4,7 @@ public class CarDemo {
     private JPanel mainPanel;
     private JTextField textField1;
     private JButton button1;
+    private JPanel panel1;
     private JPanel northPane1;
 
     public static void main(String[] args) {
@@ -11,5 +12,9 @@ public class CarDemo {
         frame.setSize(600, 400);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
