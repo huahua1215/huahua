39519fa4115c75a060aec89d27849690f410df51
huahua1215 <s06u.vm0clgj9@gmail.com>
2021-06-01 16:02:44 +0800

    homework

---
+++ src/com/hau/o/car/CarDemo.java
@@ -0,0 +1,15 @@
+package com.hau.o.car;
+import javax.swing.*;
+public class CarDemo {
+    private JPanel mainPanel;
+    private JTextField textField1;
+    private JButton button1;
+    private JPanel northPane1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(600, 400);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
